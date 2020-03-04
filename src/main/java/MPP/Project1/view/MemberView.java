d202484a09310449792deee3ee1ad646bd7ee8db
Lucas Arthur Penz <lucas@Lucass-MacBook-Pro.local>
2020-03-03 21:54:42 -0600

    Segundo

---
+++ src/main/java/MPP/Project1/view/MemberView.java
@@ -0,0 +1,47 @@
+package MPP.Project1.view;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Panel;
+import java.awt.GridLayout;
+
+public class MemberView {
+
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MemberView window = new MemberView();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public MemberView() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new GridLayout(4, 3, 0, 0));
+	}
+
+}
